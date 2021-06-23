877d996a27606c507c555d88f0d161b76b4a69e3
Samet Çetin <samet.cetin@n2mobil.com.tr>
2021-06-23 12:04:05 +0300

    feat: set form options

---
+++ src/components/NewProduct.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="row">
+      <div class="card offset-2 col-md-3">
+        <div class="card-body tex-center d-flex align-items-center flex-column">
+          <img height="128" class="img-responsive text-center mb-3"
+               :src="product.selectedImage == null ? '/src/assets/default.png' : product.selectedImage">
+          <input ref="file" type="file" style="display: none;" @change="onChange($event)" class="form-control">
+          <button class="btn btn-outline-secondary " type="button" @click="$refs.file.click()">Choose Product Image</button>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="col-md-11 card">
+          <div class="card-body">
+            <div class="form-group">
+              <label>Product Name</label>
+              <input type="text" v-model="product.title" class="form-control" placeholder="enter name">
+            </div>
+            <div class="row">
+              <div class="form-group col-md-6">
+                <label>Product Count</label>
+                <input type="text" v-model="product.count" class="form-control" placeholder="enter count">
+              </div>
+              <div class="form-group col-md-6">
+                <label>Product Price</label>
+                <input type="text" v-model="product.price" class="form-control" placeholder="enter price">
+              </div>
+            </div>
+            <button @click="addProduct" class="btn btn-outline-info btn-block">Add Product</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data: function(){
+            return{
+                product : {
+                    title: null,
+                    count: null,
+                    price: null,
+                    totalPrice: null,
+                    selectedImage: null,
+
+                },
+            }
+        },
+        methods: {
+            onChange(e) {
+                const file = e.target.files[0];
+                this.product.selectedImage = URL.createObjectURL(file);
+            },
+            addProduct(){
+                this.product.totalPrice = this.product.count * this.product.price;
+                console.log(this.product);
+            },
+        },
+        
+    }
+</script>
---
+++ src/components/Products.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row product-container">
+      <app-product v-bind:key="product" v-for="product in productList">
+        <img class="card-img-top" src="/src/assets/default.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <small><strong>Adet : </strong> 1</small><br>
+          <small><strong>Fiyat : </strong> 10</small><br>
+          <small><strong>Tutar : </strong> 10</small>
+        </div>
+      </app-product>
+    </div>
+</template>
+
+<script>
+    import Product from './Product.vue';
+    export default {
+        components: {
+            appProduct: Product,
+        },
+        data: function(){
+            return{
+                productList: [],
+            }
+        },
+    }
+    
+</script>
