dcd6bb05143b0ae6299bdc34d57d98f8888741c8
Samet Çetin <samet.cetin@n2mobil.com.tr>
2021-06-23 10:41:12 +0300

    feat: created product project

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,116 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <h1 class="text-center">Ürün Ekleme Uygulaması</h1>
+    <hr>
+    <div class="row">
+      <div class="card offset-2 col-md-3">
+        <div class="card-body tex-center d-flex align-items-center flex-column">
+          <img height="128" class="img-responsive text-center mb-3"
+               :src="product.selectedImage == null ? '/src/assets/default.png' : product.selectedImage">
+          <input ref="file" type="file" style="display: none;" @change="onChange($event)" class="form-control">
+          <button class="btn btn-outline-secondary " type="button" @click="$refs.file.click()">Resim Seç</button>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="col-md-11 card">
+          <div class="card-body">
+            <div class="form-group">
+              <label>Ürün Adı</label>
+              <input type="text" class="form-control" placeholder="adını giriniz">
+            </div>
+            <div class="row">
+              <div class="form-group col-md-6">
+                <label>Ürün Adeti</label>
+                <input type="text" class="form-control" placeholder="adetini giriniz">
+              </div>
+              <div class="form-group col-md-6">
+                <label>Ürün Fiyatı</label>
+                <input type="text" class="form-control" placeholder="fiyatını giriniz">
+              </div>
+            </div>
+            <button class="btn btn-outline-info btn-block">Ekle!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br>
+    <div class="progress">
+      <div class="progress-bar bg-info" role="progressbar" style="width: 0%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+        0/10
+      </div>
+    </div>
+    <br><br>
+    <h3 class="text-center">Eklenen Ürünlerin Listesi</h3>
+    <hr>
+    <div class="row product-container">
+      <div class="col-md-2 card">
+        <img class="card-img-top" src="/src/assets/default.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <small>
+            <strong>Adet : </strong> 1
+          </small>
+          <br>
+          <small>
+            <strong>Fiyat : </strong> 10
+          </small>
+          <br>
+          <small>
+            <strong>Tutar : </strong> 10
+          </small>
+        </div>
+      </div>
+
+
+    </div>
+
   </div>
+
+
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    data() {
+      return {
+        imageList: [],
+        product : {
+            selectedImage: null
+        }
+      }
+    },
+    methods: {
+      onChange(e) {
+        const file = e.target.files[0];
+        this.product.selectedImage = URL.createObjectURL(file);
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: aliceblue;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  .card{
+    margin-right: 5px;
+    margin-bottom: 5px;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .card:last-child{
+    margin-right: 0px;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .col-md-2{
+    max-width: 15.666667%!important;
+  }
 
-a {
-  color: #42b983;
-}
+  .product-container{
+    margin-left: 15px;
+  }
+
 </style>
+
